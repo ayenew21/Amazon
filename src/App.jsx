27a5c5608bcbd26600e88f0d995f076e7af6e2e3
ayenew21 <learn4396@gmail.com>
2025-02-18 16:05:36 +0300

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./index.css";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
-    
+      <Header />
     </>
   );
 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import classes from "./Header.module.css";
+import { SlLocationPin } from "react-icons/sl";
+import { BsSearch } from "react-icons/bs";
+import { BiCart } from "react-icons/bi";
+import LowerHeader from "./LowerHeader";
+
+const Header = () => {
+  return (
+    <>
+      <div className={classes.header_container}>
+        {/* logo section */}
+        <div className={classes.logo_container}>
+          <a href="/">
+            <img
+              src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+              alt="Amazon logo"
+            />
+          </a>
+
+          {/* delivery */}
+          <div className={classes.delivery}>
+            <span>
+              <SlLocationPin />
+            </span>
+            <div>
+              <p>Delivere to</p>
+              <span>Ethiopia</span>
+            </div>
+          </div>
+        </div>
+
+        {/* search section*/}
+
+        <div className={classes.search}>
+          <select name="" id="">
+            <option value="">All</option>
+          </select>
+          <input type="text" />
+          <BsSearch size={25} />
+        </div>
+        {/* right side link / other section*/}
+        <div className={classes.order_container}>
+          <a className={classes.language}>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1024px-Flag_of_the_United_States.svg.png"
+              alt=""
+            />
+            <section>
+              <option value="">EN</option>
+            </section>
+          </a>
+          {/* three comp;onents */}
+          <a href="">
+            <div>
+              <p>Sign In</p>
+              <span>Account &Lists</span>
+            </div>
+          </a>
+          {/* Orders */}
+          <a href="">
+            <p>returns</p>
+            <span>& Orders</span>
+          </a>
+          {/* Cart */}
+          <a href="" className={classes.cart}>
+            <BiCart size={35} />
+            {/* icon */}
+            <span>0</span>
+          </a>
+        </div>
+      </div>
+      <LowerHeader />
+    </>
+  );
+};
+
+export default Header;
