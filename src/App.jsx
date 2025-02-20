f0119082036d1dcdf958f8ae566fef51c2e8b105
ayenew21 <learn4396@gmail.com>
2025-02-20 15:20:36 +0300

    Category added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header/Header";
 import "./index.css";
 import CarouselEffect from "./components/Carousel/Carousel";
+import Category from "./components/Category/Category";
 
 const App = () => {
   return (
     <>
       <Header />
       <CarouselEffect />
+      <Category />
     </>
   );
 };
---
+++ src/components/Category/CategoryFullInfos.jsx
@@ -0,0 +1,28 @@
+const categoryInfos = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imageLink:
+      "https://image.shutterstock.com/image-photo/modern-tv-set-isolated-on-260nw-2548119265.jpg",
+  },
+  {
+    title: "Discover fashion trends",
+    name: "womens-fashion",
+    imageLink:
+      "https://www.travelandleisure.com/thmb/rYQ-zxRTAe2z8ERzjE-UbgF6Obc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/amazons-best-new-fashion-arrivals-this-month-tout-7410d8059f9242b7b06d398d561b9884.jpg",
+  },
+  {
+    title: "Men's clothing",
+    name: "mens-clothing",
+    imageLink:
+      "https://www.chapsandco.com/ae/wp-content/uploads/sites/12/2022/05/MFT1.jpg",
+  },
+  {
+    title: "Jewelry",
+    name: "jewelry",
+    imageLink:
+      "https://i.etsystatic.com/14734809/r/il/73093d/4276656296/il_570xN.4276656296_8j1v.jpg",
+  },
+];
+
+export { categoryInfos };
