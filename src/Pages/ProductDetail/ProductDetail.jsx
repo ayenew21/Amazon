7556494f0b76c5f521ec727c36188e5defdb6e78
ayenew21 <learn4396@gmail.com>
2025-02-22 11:10:55 +0300

    Detail page Routing

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./ProductDetail.module.css";
 import Layout from "../../components/Layout/Layout";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { productUrl } from "../../Api/endPoint";
+import ProductCard from "../../components/Product/ProductCard";
+import { useState } from "react";
+import Loader from "../../components/Loader/Loader";
 
 function ProductDetail() {
+  const [product, setProduct] = useState({});
+  const [isLoading, setisLoading] = useState(false);
+  const { productId } = useParams();
+  useEffect(() => {
+    setisLoading(true);
+    axios
+      .get(`${productUrl}/products/${productId}`)
+      .then((res) => {
+        setProduct(res.data);
+        setisLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setisLoading(false);
+      });
+  }, []);
   return (
     <Layout>
-      <div>ProductDetail</div>;
+      {isLoading ? <Loader /> : <ProductCard product={product} />}
     </Layout>
   );
 }
