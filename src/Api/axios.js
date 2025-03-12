c2f71eb73b2f7d614048a0c73fca32db68af1f91
ayenew21 <learn4396@gmail.com>
2025-03-12 11:06:16 +0300

    axios baseURL added using render.com

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-b55f7/us-central1/api",
+  // for local firebase functions
+  // baseURL: "http://127.0.0.1:5001/clone-b55f7/us-central1/api",
+
+  // deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-j96e.onrender.com",
 });
 
 export { axiosInstance };
